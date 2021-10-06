fedb41f133337ba329ec5ce9d3927e06bc4b5083
Rangasai K R <sai.ranga3@gmail.com>
2021-10-06 20:15:32 +0530

    Exercise 3.5

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 express = require('express')
 app = express()
 
+app.use(express.json())
 
 let notes = [
     { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+      id: 1,
+      name: "Arto Hellas", 
+      number: "040-123456"
     },
     { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+      id: 2,
+      name: "Ada Lovelace", 
+      number: "39-44-5323523"
     },
     { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+      id: 3,
+      name: "Dan Abramov", 
+      number: "12-43-234345"
     },
     { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+      id: 4,
+      name: "Mary Poppendieck", 
+      number: "39-23-6423122"
     }
 ]
 
@@ -47,6 +48,15 @@ app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(item => item.id !== id)
   response.status(200).end()
+})
+
+app.post("/api/persons", (request, response) => {
+  const item = request.body
+  notes = notes.concat({
+    id : Math.floor(Math.random() * 1000000),
+    ...item
+  })
+  response.send(notes)
 })
 
 const PORT = 3001
