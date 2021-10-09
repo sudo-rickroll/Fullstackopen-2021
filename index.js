2d4feb24a9f20a170da8744cdab30b584619af17
Rangasai K R <sai.ranga3@gmail.com>
2021-10-09 17:40:09 +0530

    Exercise 3.9

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const morgan = require('morgan')
 
 express = require('express')
+cors = require('cors')
 app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
